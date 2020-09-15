096c8b99de2801550c59d508d0d2b80395f3455e
tbs99 <timsregisters@gmail.com>
2020-09-15 21:14:11 +0300

    new Servlet with Post and Get

---
+++ src/main/java/serv/HelloServlet.java
@@ -0,0 +1,26 @@
+package serv;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    private String responseTemplate = "Hello world";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        process(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        process(request, response);
+    }
+
+    private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(200);
+        response.getWriter().write(responseTemplate);
+    }
+}
